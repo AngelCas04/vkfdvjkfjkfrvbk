c3db91a8eb49006f6926efd095b5278cf77a0d5d
AngelCas04 <87144434+AngelCas04@users.noreply.github.com>
2021-07-08 13:16:18 -0600

    itx

---
+++ Css/calendario.js
@@ -0,0 +1,29 @@
+var CALENDAR = function () { 
+	    var wrap, label,  
+	            months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]; 
+ 
+	    function init(newWrap) { wrap     = $(newWrap || "#cal"); 
+	label    = wrap.find("#label"); 
+	wrap.find("#prev").bind("click.calendar", function () { switchMonth(false); }); 
+	wrap.find("#next").bind("click.calendar", function () { switchMonth(true);  }); 
+	label.bind("click", function () { switchMonth(null, new Date().getMonth(), new Date().getFullYear()); });        
+	label.click();
+ 
+	    } 
+ 
+	    function switchMonth(next, month, year) { var curr = label.text().trim().split(" "), calendar, tempYear =  parseInt(curr[1], 10); 
+	month = month || ((next) ? ( (curr[0] === "December") ? 0 : months.indexOf(curr[0]) + 1 ) : ( (curr[0] === "January") ? 11 : months.indexOf(curr[0]) - 1 )); 
+	year = year || ((next && month === 0) ? tempYear + 1 : (!n
+ 
+	
+ function createCal(year, month) { 
+	    	
+ 
+	    } 
+	    createCal.cache = {}; 
+	    return { 
+	        init : init, 
+	        switchMonth : switchMonth, 
+	        createCal   : createCal 
+	    }; 
+	};
